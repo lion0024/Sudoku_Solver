55edae8d91899c764a1216c91f7b19a746203815
正晃 <lion0024.xyz@gmail.com>
2021-01-01 21:50:13 +0900

    数独ソルバーの実装

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,9 +4,21 @@ namespace Sample
 {
     class Program
     {
+        //public static void rec()
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 盤面を二次元配列で表し、-1で初期化する
+            int[][] field = new int[9][];
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                field[i] = new int[9];
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    field[i][j] = -1;
+                }
+            }
+
         }
     }
 }
